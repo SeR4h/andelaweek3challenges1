b3b0347081ecdb9e15902281a9f49af4fa052134
SeR4h <40196738+SeR4h@users.noreply.github.com>
2019-11-24 22:41:07 +0300

    Sorting function

---
+++ arraySort.js
@@ -0,0 +1,22 @@
+function arraySort(a, b) {
+    return b - a
+    //return a<b?1 : a>b?-1 : 0
+}
+
+var x = [1, 4, 7, "x", 3, 5, 8, "t", 0, 6, 10, "a"];
+x.sort(arraySort)
+x.reverse()
+i = 0
+while (i < x.length) {
+    if (isNaN(x[i])) {
+        characters.push(x[i]);
+    }
+    if (x[i] % 2 == 0) {
+        evenNumbers.push(x[i]);
+    }
+    if (x[i] % 2 == 1) {
+        oddNumbers.push(x[i]);
+    }
+    i++;
+}
+console.log(props)
