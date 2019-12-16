3f071e7f4f31b70c02bde0bbc2c3f2a93fb5c174
SeR4h <40196738+SeR4h@users.noreply.github.com>
2019-12-16 16:16:51 +0300

    updated arraySort

--- arraySort.js
+++ arraySort.js
@@ -3,29 +3,37 @@ var oddNumbers = new Array();
 var characters = new Array();
 
 let numbers = {
-    evens: evenNumbers,
-    odds: oddNumbers,
-    chars: characters,
+    'evens': evenNumbers,
+    'odds': oddNumbers,
+    'chars': characters,
 }
 
 function arraySort(myArray) {
-    for (var i = 0; i < myArray.length; i++) {
-        if (isNaN(myArray[i])) {
-            characters.push(myArray[i]);
-        }
-        if (myArray[i] % 2 == 0) {
-            evenNumbers.push(myArray[i]);
-        }
-        if (myArray[i] % 2 == 1) {
-            oddNumbers.push(myArray[i]);
-        }
-    }
-    evenNumbers.sort((a, b) => a - b);
-    oddNumbers.sort((a, b) => a - b);
-    characters.sort();
 
-    console.log(numbers);
+    if (typeof myArray === 'string') {
+        return 'invalid input';
+    }
+    else if (typeof myArray === 'number') {
+        return 'invalid input';
+    }
+    else {
+        for (var i = 0; i < myArray.length; i++) {
+            if (isNaN(myArray[i])) {
+                characters.push(myArray[i]);
+            }
+            if (myArray[i] % 2 == 0) {
+                evenNumbers.push(myArray[i]);
+            }
+            if (myArray[i] % 2 == 1) {
+                oddNumbers.push(myArray[i]);
+            }
+        }
+        evenNumbers.sort((a, b) => a - b);
+        oddNumbers.sort((a, b) => a - b);
+        characters.sort();
+    }
+    return numbers;
 }
-/*var x = [1, 4, 7, "x", 3, 5, 8, "a", 0, 6, 10, "t"];
+/*var x = [1, 4, 7, 'x', 3, 5, 8, 'a', 0, 6, 10, 't'];
 arraySort(x)*/
 module.exports = arraySort;
