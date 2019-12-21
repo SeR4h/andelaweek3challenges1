6b2bb9afcf58a76de14b2a16e7f65875edfc0263
SeR4h <40196738+SeR4h@users.noreply.github.com>
2019-12-21 13:59:15 +0300

    resolved changes

--- arraySort.js
+++ arraySort.js
@@ -12,19 +12,18 @@ function arraySort(myArray) {
     if (!Array.isArray(myArray)) {
         throw new Error('invalid input')
     }
-    else {
-        for (var i = 0; i < myArray.length; i++) {
-            // (Number.isNaN()) returns an empty chars object
-            if (isNaN(myArray[i])) {
-                characters.push(myArray[i]);
-            }
-            if (myArray[i] % 2 == 0) {
-                evenNumbers.push(myArray[i]);
-            }
-            if (myArray[i] % 2 == 1) {
-                oddNumbers.push(myArray[i]);
-            }
+    for (var i = 0; i < myArray.length; i++) {
+        // (Number.isNaN()) returns an empty chars object
+        if (isNaN(myArray[i])) {
+            characters.push(myArray[i]);
         }
+        if (myArray[i] % 2 == 0) {
+            evenNumbers.push(myArray[i]);
+        }
+        if (myArray[i] % 2 == 1) {
+            oddNumbers.push(myArray[i]);
+        }
+
         evenNumbers.sort((a, b) => a - b);
         oddNumbers.sort((a, b) => a - b);
         characters.sort();
